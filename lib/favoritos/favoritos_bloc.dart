42c4f25b97fdee8afeab10fe7db62dea56b6e9be
rlecheta <rlecheta@livetouch.com.br>
2019-06-13 21:21:48 -0300

    First commit

--- lib/favoritos/favoritos_bloc.dart
+++ lib/favoritos/favoritos_bloc.dart
@@ -5,18 +5,12 @@ import 'package:flutter_movies_udemy/utils/response.dart';
 import 'package:rxdart/rxdart.dart';
 
 class FavoritosBloc extends BlocBase {
-  // progress
-  final _progress = BehaviorSubject<bool>();
-
-  get progressStream => _progress.stream;
-
   // stream
   final _movies = BehaviorSubject<Response<List<Movie>>>();
 
   get moviesStream => _movies.stream;
 
   Future fetch({bool isRefresh = false}) async {
-    _progress.sink.add(true);
 
     try {
       if (isRefresh) {
@@ -26,18 +20,16 @@ class FavoritosBloc extends BlocBase {
       final db = MovieDB.getInstance();
       final list = await db.getMovies();
       final movies = Response(true, result: list);
-      print("< sql ${movies.result.length}");
 
       _movies.sink.add(movies);
 
       return movies;
     } finally {
-      _progress.sink.add(false);
+
     }
   }
 
   close() {
-    _progress.close();
     _movies.close();
   }
 }
--- lib/movies/movies_bloc.dart
+++ lib/movies/movies_bloc.dart
@@ -6,10 +6,6 @@ import 'package:rxdart/rxdart.dart';
 import 'movie.dart';
 
 class MoviesBloc extends BlocBase {
-  // progress
-  final _progressController = BehaviorSubject<bool>();
-
-  get progressStream => _progressController.stream;
 
   // stream
   final _moviesController = BehaviorSubject<Response<List<Movie>>>();
@@ -17,28 +13,20 @@ class MoviesBloc extends BlocBase {
   get moviesStream => _moviesController.stream;
 
   Future fetch({bool isRefresh = false}) async {
-    _progressController.sink.add(true);
 
-    try {
-      if (isRefresh) {
-        _moviesController.sink.add(null);
-      }
-
-      final movies = await MoviesApi.getMovies();
-
-      _moviesController.sink.add(movies);
-
-      return movies;
-    } finally {
-      _progressController.sink.add(false);
+    if (isRefresh) {
+      _moviesController.sink.add(null);
     }
+
+    final movies = await MoviesApi.getMovies();
+
+    _moviesController.sink.add(movies);
+
+    return movies;
   }
 
   @override
   void dispose() {
-    print("dispose");
-
-    _progressController.close();
     _moviesController.close();
 
     super.dispose();
